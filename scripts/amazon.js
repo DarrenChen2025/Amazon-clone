f6c66c8666fb6c5fcc54ae1c128cb803fa966930
DarrenChen2025 <Chen.darren@ufl.edu>
2023-09-14 11:22:28 -0400

    Add function to format total cost and fixed radio

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,6 +2,7 @@
 
 import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
 let productsHTML = '';
 
@@ -28,7 +29,7 @@ products.forEach((product) => {
             </div>
 
             <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
             </div>
 
             <div class="product-quantity-container">
---
+++ scripts/utils/money.js
@@ -0,0 +1,6 @@
+
+
+
+export function formatCurrency(priceCents) {
+    return (priceCents / 100).toFixed(2);
+}
